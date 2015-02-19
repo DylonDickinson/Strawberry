85521fafab99ce8ec9e5135de5d7252475846f45
arrietaalex13 <Alex@Alexs-MacBook-Pro.local>
2015-02-19 14:37:07 -0800

    Added text files to read from

---
+++ Project 1 - Test/BasicMember.cpp
@@ -0,0 +1,10 @@
+/*
+ * BasicMember.cpp
+ *
+ *  Created on: Feb 19, 2015
+ *      Author: Alex
+ */
+
+
+
+
---
+++ Project 1 - Test/BasicMember.h
@@ -0,0 +1,23 @@
+/*
+ * BasicMember.h
+ *
+ *  Created on: Feb 19, 2015
+ *      Author: Alex
+ */
+
+#ifndef BASICMEMBER_H_
+#define BASICMEMBER_H_
+
+class BasicMember
+{
+public:
+
+
+
+private:
+
+};
+
+
+
+#endif /* BASICMEMBER_H_ */
