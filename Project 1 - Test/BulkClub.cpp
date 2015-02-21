3459ded6da1180449f92b9496847dcdde48e8e9f
arrietaalex13 <Alex@Alexs-MacBook-Pro.local>
2015-02-21 09:59:28 -0800

    Added skeleton for bulk club class

---
+++ Project 1 - Test/BulkClub.cpp
@@ -0,0 +1,17 @@
+/*
+ * BulkClub.cpp
+ *
+ *  Created on: Feb 21, 2015
+ *      Author: Alex
+ */
+
+#include "BulkClub.h"
+
+BulkClub::BulkClub()
+{
+
+}
+
+BulkClub::~BulkClub() {}
+
+
---
+++ Project 1 - Test/BulkClub.h
@@ -0,0 +1,24 @@
+/*
+ * BulkClub.h
+ *
+ *  Created on: Feb 21, 2015
+ *      Author: Alex
+ */
+
+#ifndef BULKCLUB_H_
+#define BULKCLUB_H_
+
+
+class BulkClub
+{
+public:
+	BulkClub();
+	virtual ~BulkClub();
+
+
+private:
+
+};
+
+
+#endif /* BULKCLUB_H_ */
