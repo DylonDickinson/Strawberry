49fe77b1f82a8f146bded5e3a2ee61eb50f526e0
Plew <plewwatson@gmail.com>
2015-02-25 18:40:59 -0800

    Adding Transaction Class

---
+++ Project 1 - Test/Transaction.cpp
@@ -0,0 +1,18 @@
+/*
+ * Transaction.cpp
+ *
+ *  Created on: Feb 25, 2015
+ *      Author: Peter
+ */
+
+#include "Transaction.h"
+
+Transaction::Transaction() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Transaction::~Transaction() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Project 1 - Test/Transaction.h
@@ -0,0 +1,17 @@
+/*
+ * Transaction.h
+ *
+ *  Created on: Feb 25, 2015
+ *      Author: Peter
+ */
+
+#ifndef TRANSACTION_H_
+#define TRANSACTION_H_
+
+class Transaction {
+public:
+	Transaction();
+	~Transaction();
+};
+
+#endif /* TRANSACTION_H_ */
