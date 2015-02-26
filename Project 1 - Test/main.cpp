811595319da501cc06a772e8fb480557fd772e19
arrietaalex13 <Alex@Alexs-MacBook-Pro.local>
2015-02-26 14:27:34 -0800

    test

--- Project 1 - Test/main.cpp
+++ Project 1 - Test/main.cpp
@@ -9,12 +9,8 @@
 
 int main()
 {
-<<<<<<< HEAD
+
 	BulkClub daStrawberryClub;
-=======
-	cout << "alex is testing branching and stuff\n";
-	cout << "poop";
->>>>>>> 167ce7271f1d4e4129d2c40f43af6a452d97d0c6
 
 	return 0;
 }
