4b0d00c4c1aef7381e4524c6f0665c49d670dad2
Plew <plewwatson@gmail.com>
2015-03-05 15:54:01 -0800

    asdf

--- Project 1 - Test/main.cpp
+++ Project 1 - Test/main.cpp
@@ -27,56 +27,14 @@ int main()
 
 
 	BulkClub daClub;
-<<<<<<< HEAD
-//	short    command1, commandSearch, commandPrint, dayPrintReport;
-=======
 	Date *datePtr;
 	string itemSearch;
 	short yearExp, monthExp;
 	short command1, commandSearch, commandPrint, dayPrintReport;
->>>>>>> alexbranch
 
 	ReadInMembers(daClub);
 	ReadInTransactions(daClub);
 
-<<<<<<< HEAD
-//	BoundaryCheck(MAIN_MENU, 0, 2);
-//
-//	switch(command1)
-//	{
-//	case SEARCH : commandSearch = BoundaryCheck(SEARCH_MENU, 0, 4);
-//					switch(commandSearch)
-//					{
-//					case NAME : // (2) output purchases
-//						break;
-//					case MEM_NUM : // (2) output purchases
-//						break;
-//					case MONTH : // (8) members exp date by month
-//						break;
-//					case ITEM : // (4) quantity and price of item
-//						break;
-//					}
-//		break;
-//
-//	case PRINT : commandPrint = BoundaryCheck(PRINT_MENU, 0, 3);
-//					switch(commandPrint)
-//					{
-//					case SALES_REPORT : dayPrintReport = BoundaryCheck("Which day would you like a report for? ", 1, 5);
-//										// (1)
-//						break;
-//					case TOTAL_PURCHASES : // (3) all purchases sorted by mem number
-//						break;
-//					case ITEMS_SOLD : // (5) items sold with total sales price
-//						break;
-//					case AMT_PAID : // (7) amt paid per year per member
-//						break;
-//					}
-//		break;
-//	case ADD_OR_DELETE : // Add or delete member
-//		break;
-//
-//	}
-=======
 	BoundaryCheck(MAIN_MENU, 0, 2);
 
 	switch(command1)
@@ -129,7 +87,6 @@ int main()
 	case ADD_OR_DELETE : // Add or delete member
 		break;
 	}
->>>>>>> alexbranch
 
 	daClub.OutputMembers();
 	daClub.OutputTransactions();
