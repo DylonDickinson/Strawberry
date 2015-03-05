26019340977d98995926e3393780f3d4a5b0ac43
arrietaalex13 <Alex@Alexs-MacBook-Pro.local>
2015-03-05 15:09:20 -0800

    main menu stuff

---
+++ Project 1 - Test/Debug/OutputMember.d
@@ -0,0 +1,14 @@
+OutputMember.d: ../OutputMember.cpp ../header.h ../BulkClub.h \
+  ../PreferredMember.h ../BasicMember.h ../Date.h ../Transaction.h
+
+../header.h:
+
+../BulkClub.h:
+
+../PreferredMember.h:
+
+../BasicMember.h:
+
+../Date.h:
+
+../Transaction.h:
